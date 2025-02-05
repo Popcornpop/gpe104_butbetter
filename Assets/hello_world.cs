f55cbfe92914c8ceac027d4855c12e0cce007c3a
Jacobey <76972952+Popcornpop@users.noreply.github.com>
2025-02-04 18:14:55 -0800

    Re-added hello world text

--- Assets/hello_world.cs
+++ Assets/hello_world.cs
@@ -7,7 +7,7 @@ public class hello_world : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World");
     }
 
     // Update is called once per frame
